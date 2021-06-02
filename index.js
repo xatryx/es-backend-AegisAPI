b3abf5033f8aab5e79ff3044b65ae1e04fcb9523
Ronan Harris <yuunme@protonmail.com>
2021-06-02 20:24:12 +0700

    added endpoints to handle guilds, channels, and messages related requests

--- index.js
+++ index.js
@@ -8,6 +8,35 @@ app.get("/", async (req, res) => {
     res.send("hi honey :)")
 })
 
+app.get("/guild/:guild_id", async (req, res) => {
+    res.send({
+        "path": `${req.path}`,
+        "guild_id": `${req.params.guild_id}`,
+        "token": `${req.query.token}`
+    })
+})
+
+app.get("/guild/:guild_id/channel/:channel_id", async (req, res) => {
+    res.send({
+        "path": `${req.path}`,
+        "guild_id": `${req.params.guild_id}`,
+        "channel_id": `${req.params.channel_id}`
+    })
+})
+
+app.get("/guild/:guild_id/channel/:channel_id/message/:message_id", async (req, res) => {
+    res.send({
+        "path": `${req.path}`,
+        "guild_id": `${req.params.guild_id}`,
+        "channel_id": `${req.params.channel_id}`,
+        "message_id": `${req.params.message_id}`,
+        "createdAt": `${req.query.created}`,
+        "neutral_score": `${req.query.neutral_score}`,
+        "abuse_score": `${req.query.abuse_score}`,
+        "hate_score": `${req.query.hate_score}`,
+    })
+})
+
 app.listen(port, () => {
     console.log(`this service run at port ${port}`);
 })
