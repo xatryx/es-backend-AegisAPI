dcf5c0411122360573ebe1395b9300c8ac76b084
Ronan Harris <yuunme@protonmail.com>
2021-06-02 19:13:35 +0700

    initiate expressjs

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 import config from 'dotenv/config'
+import express from 'express'
+
+const app = express()
+const port = process.env.port || 3333
+
+app.get("/", async (req, res) => {
+    res.send("hi honey :)")
+})
+
+app.listen(port, () => {
+    console.log(`this service run at port ${port}`);
+})
